f6584ec201667607186e9464b25d92de087dd2ac
Apia <beekayt274@gmail.com>
2025-10-10 13:22:21 +1300

    grid, hotkeys

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -2,17 +2,30 @@ extends Control
 class_name Editor
 
 @onready var level:Level = %level
+@onready var modes:Modes = %modes
+@onready var levelViewportCont:SubViewportContainer = %levelViewportCont
 
 enum Mode {SELECT, TILE, KEY, DOOR, PASTE}
 var mode:Mode = Mode.SELECT
 
-func _guiInput(event:InputEvent) -> void:
-    if event is InputEventMouse:
-        var worldPosition:Vector2 = event.position - %levelViewportCont.position
-        var tilePosition:Vector2i = Vector2i(worldPosition) / Vector2i(32,32)
-        match mode:
-            Mode.TILE:
-                if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-                    level.tiles.set_cell(tilePosition,1,Vector2i(1,1))
-                elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-                    level.tiles.erase_cell(tilePosition)
+func _input(event:InputEvent) -> void:
+	if event is InputEventMouse:
+		var worldPosition:Vector2 = event.position - levelViewportCont.position
+		var tilePosition:Vector2i = Vector2i(worldPosition) / Vector2i(32,32)
+		match mode:
+			Mode.TILE:
+				if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+					level.tiles.set_cell(tilePosition,1,Vector2i(1,1))
+				elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+					level.tiles.erase_cell(tilePosition)
+	elif event is InputEventKey:
+		print(event)
+		if event.pressed:
+			hotkey(event)
+
+func hotkey(event:InputEventKey):
+	match event.keycode:
+		KEY_ESCAPE: modes.setMode(Mode.SELECT)
+		KEY_T: modes.setMode(Mode.TILE)
+		KEY_K: modes.setMode(Mode.KEY)
+		KEY_D: modes.setMode(Mode.DOOR)
--- scripts/level.gd
+++ scripts/level.gd
@@ -1,4 +1,18 @@
 extends Node2D
 class_name Level
 
+@onready var editor:Editor = get_node("/root/editor")
 @onready var tiles:TileMapLayer = %tiles
+
+func _draw() -> void:
+	var sizeTiles:Vector2i = ceil(editor.levelViewportCont.size / 32)
+	for x in sizeTiles.x:
+		draw_line(
+			Vector2(32*x,0),
+			Vector2(32*x,editor.levelViewportCont.size.y),
+			Color.GRAY)
+	for y in sizeTiles.y:
+		draw_line(
+			Vector2(0,32*y),
+			Vector2(editor.levelViewportCont.size.x,32*y),
+			Color.GRAY)
